29ee8877c79908f11e8ca1fb4054024ea0cc398f
dRcmd <76197823+dRcmd@users.noreply.github.com>
2022-04-21 22:33:23 +0300

    first commit

--- real-estate-web/Models/Database/About.cs
+++ real-estate-web/Models/Database/About.cs
@@ -9,7 +9,7 @@ namespace real_estate_web.Models.Database
           egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, 
           auctor sit amet aliquam vel, ullamcorper sit amet ligula.";
 
-        public string BodyContentB { get; set; } = @"Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, 
+        public string? BodyContentB { get; set; } = @"Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget consectetur sed, 
         convallis at tellus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. 
         Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.";
     }
